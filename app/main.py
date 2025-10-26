3a10ab59087dd91d1d67af317ad87571fdd27013
ivan <ivanventik@gmail.com>
2025-10-26 17:06:31 +0200

    Soullution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+
+    person_instances = []
+
+    for pers in people:
+        person_instances.append(Person(name=pers["name"], age=pers["age"]))
+
+    for pers in people:
+
+        person = Person.people[pers["name"]]
+
+        if "wife" in pers and pers["wife"] is not None:
+            person.wife = Person.people[pers["wife"]]
+
+        if "husband" in pers and pers["husband"] is not None:
+            person.husband = Person.people[pers["husband"]]
+
+    return person_instances
